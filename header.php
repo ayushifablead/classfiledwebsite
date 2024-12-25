f2e902e2240130a9031ee073ddb19ed2874b7c67
Nehakhorasiya007 <neha.fablead@gmail.com>
2024-12-25 10:14:23 +0530

    changes

--- header.php
+++ header.php
@@ -41,17 +41,18 @@
     <div class="humberger__menu__overlay"></div>
     <div class="humberger__menu__wrapper">
         <div class="humberger__menu__logo">
-            <a href="#"><img src="img/logo.png" alt=""></a>
+            <!-- <a href="#"><img src="img/logo.png" alt=""></a> -->
+            <h4 onclick="window.location.href='index.php'">Classified</h4>
         </div>
-        <div class="humberger__menu__cart">
+        <!-- <div class="humberger__menu__cart">
             <ul>
                 <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
                 <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
             </ul>
             <div class="header__cart__price">item: <span>$150.00</span></div>
-        </div>
+        </div> -->
         <div class="humberger__menu__widget">
-            <div class="header__top__right__language">
+            <!-- <div class="header__top__right__language">
                 <img src="img/language.png" alt="">
                 <div>English</div>
                 <span class="arrow_carrot-down"></span>
@@ -59,37 +60,81 @@
                     <li><a href="#">Spanis</a></li>
                     <li><a href="#">English</a></li>
                 </ul>
-            </div>
+                
+            </div> -->
             <div class="header__top__right__auth">
-                <a href="login.php"><i class="fa fa-user"></i> Login</a>
+                <!-- <a href="login.php"><i class="fa fa-user"></i> Login</a> -->
+                <div class="col-sm-6">
+                <div class="hero__categories">
+                        <div class="hero__categories__all">
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+
+                            <span>Location</span>
+                        </div>
+                        <ul style="display:none;">
+                            <li><input type="text" class="form-control" placeholder="Search"></li>
+                            <li><a href="#">India</a></li>
+                            <li><a href="#">Ahmedabad</a></li>
+                            <li><a href="#">Bangalore</a></li>
+                            <li><a href="#">Chandigarh</a></li>
+                            <li><a href="#">Chennai</a></li>
+                          
+                        </ul>
+
+                        
+                    </div>
+
+                    <div class="hero__search">
+                        <div class="hero__search__form">
+                            <form action="#" class="hero__search__form1">
+                                <div class="hero__search__categories">
+                                    Categories<i class="fa fa-angle-down" aria-hidden="true"></i>
+
+                                    <!-- <span class="arrow_carrot-down"></span> -->
+                                </div>
+                                <ul style="display:none;">
+                            <!-- <li><input type="text" class="form-control" placeholder="Search"></li> -->
+                            <li><a href="#">India</a></li>
+                            <li><a href="#">Ahmedabad</a></li>
+                            <li><a href="#">Bangalore</a></li>
+                            <li><a href="#">Chandigarh</a></li>
+                            <li><a href="#">Chennai</a></li>
+                          
+                        </ul>
+
+                                <input type="text" placeholder="Search">
+                                <button type="submit" class="site-btn"><i class="fa fa-search" aria-hidden="true"></i>                                </button>
+                            </form>
+                        </div>
+                      
+                    </div>
+                </div>
             </div>
         </div>
         <nav class="humberger__menu__nav mobile-menu">
             <ul>
-                <li class="active"><a href="./index.php">Home</a></li>
-                <!-- <li class=""><a href="./about-us.php">About Us</a></li> -->
-                <li><a href="./shop-grid.php">Product</a></li>
-                <!-- <li><a href="#">Pages</a>
-                    <ul class="header__menu__dropdown">
-                        <li><a href="./shop-details.php">Shop Details</a></li>
-                        <li><a href="./shoping-cart.php">Shoping Cart</a></li>
-                        <li><a href="./checkout.php">Check Out</a></li>
-                        <li><a href="./blog-details.php">Blog Details</a></li>
-                    </ul>
-                </li> -->
-                <li><a href="./category.php">Category </a></li>
-                <li><a href="./blog.php">Blog</a></li>
-                <li><a href="./faqs.php">Faqs</a></li>
-                <li><a href="./contact.php">Contact</a></li>
+            <li class=""><a href="./index.php">Home</a></li>
+                            <!-- <li class=""><a href="./about-us.php">About Us</a></li> -->
+                            <li><a href="./shop-grid.php">Ads</a></li>
+                            <!-- <li><a href="#">Pages</a>
+                                <ul class="header__menu__dropdown">
+                                    <li><a href="./shop-details.php">Shop Details</a></li>
+                                    <li><a href="./shoping-cart.php">Shoping Cart</a></li>
+                                    <li><a href="./checkout.php">Check Out</a></li>
+                                    <li><a href="./blog-details.php">Blog Details</a></li>
+                                </ul>
+                            </li> -->
+                            <li><a href="./category.php">Categories</a></li>
+                          <li><a href="login.php">Login</a></li>
             </ul>
         </nav>
         <div id="mobile-menu-wrap"></div>
-        <div class="header__top__right__social">
+        <!-- <div class="header__top__right__social">
             <a href="#"><i class="fa fa-facebook"></i></a>
             <a href="#"><i class="fa fa-twitter"></i></a>
             <a href="#"><i class="fa fa-linkedin"></i></a>
             <a href="#"><i class="fa fa-pinterest-p"></i></a>
-        </div>
+        </div> -->
         <div class="humberger__menu__contact">
             <ul>
                 <li><i class="fa fa-envelope"></i> info@gmail.com</li>
@@ -139,7 +184,7 @@
         </div> -->
         <div class="container-fluid header__top1">
             <div class="row">
-                <div class="col-lg-2 ">
+                <div class="col-lg-2">
                     <div class="header__logo">
                         <!-- <a href="./index.php"><img src="img/logo.png" alt=""></a> -->
                          <h4 onclick="window.location.href='index.php'">Classified</h4>
