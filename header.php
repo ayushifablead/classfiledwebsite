94110e0d41941ff8fae00e0a67db856fba5ffeed
Nehakhorasiya007 <neha.fablead@gmail.com>
2024-12-26 16:28:22 +0530

    changes

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Classified</title>
-    <!-- <link rel="icon" href="favicon.ico"  type="image/x-icon" /> -->
-    <link rel="shortcut icon" href="http://localhost/classified/favicon.ico" />
+    <!-- <link rel="icon" href="img/favicon.ico"  type="image/x-icon" /> -->
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
