02b2eab324d4b15ea2624a413359084532fd0284
ayushifablead <ayushifablead@gmail.com>
2024-12-26 14:57:27 +0530

    first commit

---
+++ cookiepolicy.php
@@ -0,0 +1,85 @@
+<?php include 'header.php'; ?> 
+
+ <!-- Breadcrumb Section Begin -->
+ <section class="breadcrumb-section set-bg" data-setbg="img/categories/banner.png">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>Cookie Policy</h2>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
+    <section class="terms_area">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                
+                <div class="col-lg-8 pl-5">
+                    <div class="about_content">
+                       
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa, a quisquam placeat:</p>
+                        <div class="sign_title">
+                            <h5 class="title mb-2">1. Lorem ipsum</h5>
+
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                   
+                   
+                       
+                        <div class="sign_title">
+                            <h5 class="title mb-2">2. Lorem ipsum</h5>
+
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                        <div class="sign_title">
+                            <h5 class="title mb-2">3. Lorem ipsum</h5>
+
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+                        <div class="pl-3">
+                            <h6 class="mb-2"><b>Some info</b></h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo expedita minima architecto adipisci atque neque libero facilis officia blanditiis provident repudiandae numquam earum dolorem hic quis ipsa</p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?> 
