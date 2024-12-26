e45214eb88a80c1521295237a66127ae7e5c9c99
Nehakhorasiya007 <neha.fablead@gmail.com>
2024-12-26 13:44:15 +0530

    changes

--- product.php
+++ product.php
@@ -19,7 +19,7 @@
     <!-- Breadcrumb Section End -->
 
     <section class="mt-3 mb-5">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row mb-3 mt-5">
         <div class="col-lg-3">
     <div class="sidebar">
@@ -258,23 +258,7 @@
                         </div> -->
                  
                         </div>
-                        <div class="sidebar__item">
-                            <h4>Price</h4>
-                            <div class="price-range-wrap">
-                                <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-                                    data-min="10" data-max="540">
-                                    <div class="ui-slider-range ui-corner-all ui-widget-header"></div>
-                                    <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
-                                    <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
-                                </div>
-                                <div class="range-slider">
-                                    <div class="price-input">
-                                        <input type="text" id="minamount">
-                                        <input type="text" id="maxamount">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                       
                         <!-- <div class="sidebar__item sidebar__item__color--option">
                             <h4>Colors</h4>
                             <div class="sidebar__item__color sidebar__item__color--white">
@@ -341,71 +325,7 @@
                                 </label>
                             </div>
                         </div> -->
-                        <div class="sidebar__item">
-                            <div class="latest-product__text">
-                                <h4>Latest Products</h4>
-                                <div class="latest-product__slider owl-carousel">
-                                    <div class="latest-prdouct__slider__item">
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="img/latest-product/lp-1.jpg" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6>Palak</h6>
-                                                <span>$30.00</span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="img/latest-product/lp-2.jpg" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6>Capsicum </h6>
-                                                <span>$30.00</span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="img/latest-product/lp-3.jpg" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6>Chicken</h6>
-                                                <span>$30.00</span>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="latest-prdouct__slider__item">
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="img/latest-product/lp-1.jpg" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6>Palak</h6>
-                                                <span>$30.00</span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="img/latest-product/lp-2.jpg" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6>Capsicum</h6>
-                                                <span>$30.00</span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="img/latest-product/lp-3.jpg" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6>Chicken</h6>
-                                                <span>$30.00</span>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                       
                     </div>
             
    </div>
@@ -431,7 +351,7 @@
                                 <option value="4">Potato</option>
                             </select><div class="nice-select" tabindex="0"><span class="current">12 Items</span><ul class="list"><li data-value="0" class="option selected">12 Items</li><li data-value="1" class="option">Some option</li><li data-value="2" class="option">Another option</li><li data-value="4" class="option">Potato</li></ul></div>
                         </div>
-                        <div class="product_tab_menu mt-50">
+                        <!-- <div class="product_tab_menu mt-50">
                             <ul class="nav justify-content-center" id="myTab" role="tablist">
                                 <li class="nav-item mr-2">
                                     <a class="active" id="grid-tab" data-toggle="tab" href="#grid" role="tab" aria-controls="grid" aria-selected="true">
@@ -444,7 +364,7 @@
                                     </a>
                                 </li>
                             </ul>
-                        </div>
+                        </div> -->
                     </div>
 
            
@@ -732,30 +652,7 @@
                                     </div>
 
 
-                                        <div class="col-lg-3 col-md-4 col-sm-6 mix vegetables fastfood">
-                                        <div class="featured__item card">
-                                            <div class="product__discount__item__pic featured__item__pic set-bg" data-setbg="img/categories/3.png">
-                                           
-                                                <ul class="featured__item__pic__hover">
-                                                    <li><a href="shop-details.php"><i class="fa fa-eye" aria-hidden="true"></i>  
-                                                </ul>
-                                            </div>
-                                            <div class="featured__item__text product-list">
-                                                <div class="ads_card_content">
-                                                <div class="meta d-flex justify-content-between">
-                                                    <p>Ram &amp; Laptop</p>
-                                                    <a class="like" href="#"><i class="fa fa-heart icon-color"></i></a>
-                                                </div>
-                                                <h6 class="title"><a href="shop-details.php">8 GB DDR4 Ram, 4th Gen</a></h6>
-                                                <p class="mb-0"><i class="fa fa-map-marker icon-color"></i> New York, USA</p>
-                                                <div class="ads_price_date d-flex justify-content-between">
-                                                    <span class="price">$399.00</span>
-                                                    <span class="date">25 Jan, 2023</span>
-                                                </div>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                       
 
 
 
